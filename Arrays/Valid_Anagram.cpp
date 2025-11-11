7b8fb49a7fe99b111ca847d09b2be9e76720bd1e
Aditya-354 <adigithub354@gmail.com>
2025-11-11 16:41:08 +0530

    Solved Valid Anagram problem

---
+++ Arrays/Valid_Anagram.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include<unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+  bool isAnagram(string s, string t)
+  {
+    if(s.size() != t.size()) return false;
+    unordered_map<char, int> count;
+    for(int i = 0; i < s.size(); i++)
+    {
+      count[s[i]]++;
+      count[t[i]]--;
+    }
+    for(auto &p : count)
+    {
+      if(p.second != 0) return false;
+    }
+    return true;
+  }
+};
+
+int main()
+{
+  Solution y;
+  string s = "rat";
+  string t = "tar";
+  bool b = y.isAnagram(s, t);
+  cout << b;
+}
